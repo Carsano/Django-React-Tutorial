8c90c548af2e4917fbaab8576e4e965e2317861f
Carsano <damien.fabiano@protonmail.com>
2025-02-22 19:39:15 +0100

    Add migration and fix typos

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Note(models.Model):
-    title = models.Charfield(max_length=100)
+    title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
