d29af44ae72918e8e60111992d4eccf22588f8c2
Carsano <damien.fabiano@protonmail.com>
2025-02-22 14:29:43 +0100

    Add class UserSerializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,3 +1,7 @@
 from django.contrib.auth.models import UserAttributeSimilarityValidator
 from rest_framework import serializers
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
