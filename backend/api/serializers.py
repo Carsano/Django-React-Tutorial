ce5a0bbfc76ee16b5d325d11899d9a66eaf9e50c
Carsano <39464038+Carsano@users.noreply.github.com>
2025-02-23 15:35:26 +0100

    Model creation (#2)
    * Add serializer.py
    
    * Add class UserSerializer
    
    * add def create
    
    * Add explain comment"
    
    * Add CreateUserView
    
    * Add url patterns
    
    * Add .gitignore
    
    * server try
    
    * Add Note class
    
    * Note NoteSerializer to the API
    
    * Delete pycache from git
    
    * Add noteListeCreate and NoteDelete Serializer
    
    * Add urls.py
    
    * Add api.urls to path
    
    * Add migration and fix typos
    
    * Add migration

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Note
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -7,9 +8,16 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ["id", "username", "password"]
         # Tells Django that we only write password when creating new user but no possibility to read the password
-        extra_kwarg = {"password": {"write_only": True}}
+        extra_kwargs = {"password": {"write_only": True}}
 
     # Validated data will be already be validated by the serializers.ModelSerializer
     def create(self, validated_data) -> User:
         user = User.objects.create_user(**validated_data)
         return user
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ["id", "title", "content", "created_at", "author"]
+        extra_kwargs = {"author": {"read_only": True}}
