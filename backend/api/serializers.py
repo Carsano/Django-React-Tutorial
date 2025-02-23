2b72ddf68b8194f8ea0888d0f30d9bddcf3a1d5a
Carsano <39464038+Carsano@users.noreply.github.com>
2025-02-23 15:25:58 +0100

    Jwt token (#1)
    * Add serializer.py
    
    * Add class UserSerializer
    
    * add def create
    
    * Add explain comment"
    
    * Add CreateUserView
    
    * Add url patterns
    
    * Add .gitignore
    
    * server try

---
+++ backend/api/serializers.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+        # Tells Django that we only write password when creating new user but no possibility to read the password
+        extra_kwarg = {"password": {"write_only": True}}
+
+    # Validated data will be already be validated by the serializers.ModelSerializer
+    def create(self, validated_data) -> User:
+        user = User.objects.create_user(**validated_data)
+        return user
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
 
 # Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    # Check all the objects before creating a User, avoid creating an User that already exists
+    queryset = User.objects.all()
+    # Tells the view the class need to be use to create a user
+    serializer_class = UserSerializer
+    # Who can actually call this to create a new user
+    permission_classes = [AllowAny]
